315be902f13b3fa8117a52fd99075fef7df99c11
jdoi <jdoi.work@gmail.com>
2019-05-21 08:35:27 +0900

    プロダクション情報対応

--- src/MyApp.hs
+++ src/MyApp.hs
@@ -1,13 +1,40 @@
 module MyApp (runApp) where 
 
-import Web.Scotty (scotty, get, notFound, text, param)
+import Web.Scotty (scotty, get, notFound, text, param, Parsable, parseParam)
+
+data Production = P765
+                | P346
+                | P315
+                | P283
+                  deriving (Show)
+
+instance Parsable Production where
+  parseParam "765" = Right P765
+  parseParam "346" = Right P346
+  parseParam "315" = Right P315
+  parseParam "283" = Right P283
+  parseParam _     = Left "Unknown production"
+
+textLn t = text $ t <> "\n"
 
 routes = do
+  -- Homepage
   get "/" $ text "hello scotty"
+
+  -- Echo words
   get "/echo/:words" $ do
     words <- param "words"
-    text $ words <> "\n"
+    textLn words
 
-  notFound $ text "404 not found"
+  -- Production info
+  get "/productions/:id" $ do
+    pro <- param "id"
+    textLn $ case pro of
+      P765 -> "Takagi"
+      P346 -> "Mishiro"
+      P315 -> "Saitou"
+      P283 -> "Amai"
+
+  notFound $ text "404 not found\n"
 
 runApp = scotty 3000 $ routes
