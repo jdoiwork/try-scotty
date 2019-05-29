b6352e9872fa2600ec671dc17f18f75f53c8900c
jdoi <jdoi.work@gmail.com>
2019-05-29 16:36:56 +0900

    よりわかりやすいfromMaybeを使うようにした

--- src/MyApp/Port.hs
+++ src/MyApp/Port.hs
@@ -2,13 +2,14 @@ module MyApp.Port (getPort) where
 
 import System.Environment (lookupEnv)
 import Text.Read (readMaybe)
+import Data.Maybe (fromMaybe)
 import Network.Wai.Handler.Warp (Port)
 
 parsePort :: Maybe String -> Maybe Port
 parsePort = (>>= readMaybe)
 
 defaultPortOr :: Maybe Port -> Port
-defaultPortOr = maybe 3000 id
+defaultPortOr = fromMaybe 3000
 
 parsePortSafe :: Maybe String -> Port
 parsePortSafe = defaultPortOr . parsePort
